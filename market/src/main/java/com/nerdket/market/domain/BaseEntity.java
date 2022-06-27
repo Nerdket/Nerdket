e94085560686e23ebca4605b7d2c83f5279fefd3
yyeopp <letsksy@naver.com>
2022-06-27 14:01:36 +0900

    [추가] JPA Auditing으로 날짜 자동생성

--- market/src/main/java/com/nerdket/market/domain/BaseEntity.java
+++ market/src/main/java/com/nerdket/market/domain/BaseEntity.java
@@ -1,35 +1,34 @@
 package com.nerdket.market.domain;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import lombok.Getter;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-	@CreatedBy
-	@Column(updatable = false)
-	private String createdBy;
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
 
-	@LastModifiedBy
-	private String lastModifiedBy;
+    @LastModifiedBy
+    private String lastModifiedBy;
 
-	@CreatedDate
-	@Column(updatable = false)
-	private LocalDateTime createdDate;
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
 
-	@LastModifiedDate
-	private LocalDateTime lastModifiedDate;
+    @LastModifiedDate
+    @Column
+    private LocalDateTime lastModifiedDate;
 }
